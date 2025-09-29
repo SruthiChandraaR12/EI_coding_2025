142fafee3b9a87a3df6e80e2cf152f839f9d53b3
SruthiChandraaR12 <sruthichandraa12@gmail.com>
2025-09-29 14:25:14 +0530

    new commit

--- Behavioural Pattern/Singleton.java
+++ Behavioural Pattern/Singleton.java
@@ -1,6 +1,6 @@
 class Singleton{
     public static void main(String[] args){
-        System.out.print("Singleton Class");
+        System.out.print("Singleton ");
         System.out.println("hello");
     }
 }
